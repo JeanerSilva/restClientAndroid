7feb735c9f9cdca8bf62f0bb406bcd988cf97459
JeanerSilva <jeanerufmg@gmail.com>
2018-03-25 03:48:49 -0600

    Tracker Object

--- app/src/main/java/com/conecta/restserver/MainActivity.java
+++ app/src/main/java/com/conecta/restserver/MainActivity.java
@@ -1,5 +1,6 @@
 package com.conecta.restserver;
 
+import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,85 +14,97 @@ import android.widget.TextView;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
     String TAG = "RestClient";
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button restGetButton = (Button) findViewById(R.id.restGetButton);
-        final EditText restUrlText = (EditText) findViewById(R.id.restUrlText);
-        restUrlText.setText("https://api.github.com/");
+        Button restGetButton = findViewById(R.id.restGetButton);
+        final EditText restUrlText = findViewById(R.id.restUrlText);
+        //restUrlText.setText("https://api.github.com/");
+        restUrlText.setText("https://coliconwg.appspot.com/pull?entity=moto");
 
-        Log.d(TAG, "onCreate " );
+
+        Log.e(TAG, "onCreate " );
 
         restGetButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 AsyncTask.execute(new Runnable() {
                     @Override
                     public void run() {
-
-                        Log.d(TAG, "GET " );
-                        URL githubEndpoint = null;
+                        final TextView restRestultText = (TextView) findViewById(R.id.restResultText);
+                        Log.e(TAG, "GET " );
+                        URL url = null;
                         try {
-                            githubEndpoint = new URL(restUrlText.getText().toString());
-                            //githubEndpoint = new URL("http://localhost:9090/colicon/buscacontrato/3");
-                        } catch (MalformedURLException e) {
-                            Log.d(TAG, "eRRO" );
+                            url = new URL(restUrlText.getText().toString());
+                           // url = new URL("https://coliconwg.appspot.com/");
+                            
+                        } catch (final MalformedURLException e) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    restRestultText.setText( e.getMessage());
+                                }
+                            });
+
+                            Log.e(TAG, "eRRO" );
                             Log.e(TAG, e.getMessage());
                             e.printStackTrace();
                         }
-                        final TextView restRestultText = (TextView) findViewById(R.id.restResultText);
+
                         // Create connection
                         try {
-
+                            Log.e(TAG, url.toString() );
                             HttpsURLConnection myConnection =
-                                    (HttpsURLConnection) githubEndpoint.openConnection();
+                                    (HttpsURLConnection) url.openConnection();
                             myConnection.setRequestProperty("User-Agent", "my-rest-app-v0.1");
-                            if (myConnection.getResponseCode() == 200) {
-                                Log.d(TAG, "CODE 200" );
+                            myConnection.setRequestMethod("GET");
+                            System.out.println("\nSending request to URL : " + myConnection);
 
+                            myConnection.addRequestProperty("entity", "moto");
+                            if (myConnection.getResponseCode() == 200) {
+                                Log.e(TAG, "CODE 200" );
+                                String lista = "";
                                 InputStream responseBody = myConnection.getInputStream();
                                 InputStreamReader responseBodyReader =
                                         new InputStreamReader(responseBody, "UTF-8");
                                 JsonReader jsonReader = new JsonReader(responseBodyReader);
-                                jsonReader.beginObject(); // Start processing the JSON object
-                               // StringBuilder value = null;
-
-
-                                while (jsonReader.hasNext()) { // Loop through all keys
-                                    String key = jsonReader.nextName(); // Fetch the next key
-                                    // if (key.equals("organization_url")) { // Check if desired key
-                                    // Fetch the value as a String
-                                    final String value  = jsonReader.nextString();
-                                   // restRestultText.setText(jsonReader.nextString().toString());
-                                    Log.d(TAG, "Value: " + value);
-
-                                    runOnUiThread(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            restRestultText.setText(restRestultText.getText() + value.toString());
-                                        }
-                                    });
-
+                                List<TrackerPos> trackerPosList = new ArrayList<>();
+                                TrackerPos trackerPos;
+                                jsonReader.beginObject();
+                                String key = jsonReader.nextName();
+                                Log.e(TAG, "key: " + key);
+                                jsonReader.beginArray();
+                                while (jsonReader.hasNext()) {
+                                    trackerPos = readerTrackerPos(jsonReader);
+                                    trackerPosList.add(trackerPos);
+                                    lista = lista + trackerPos.toString();
                                 }
-                                //restRestultText.setText(value);
-                                //restRestultText.setText("fff");
+                                final String listacompleta = lista;
+                                jsonReader.endArray();
                                 jsonReader.close();
-
                                 myConnection.disconnect();
 
-
-
-                            } else {
-                                Log.d(TAG, "codigo diferente de 200: " );
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        restRestultText.setText(restRestultText.getText() + listacompleta);
+                                    }
+                                });
+                           } else {
+                                Log.e(TAG, "codigo diferente de 200: " );
                             }
 
                         } catch (final IOException e) {
@@ -110,18 +123,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-
-
-
-
-
-
-
-
     }
 
+    public TrackerPos readerTrackerPos (JsonReader reader) throws IOException {
+        String pos = null;
+        String time = null;
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals("pos")) {
+                pos = reader.nextString();
+            } else if (name.equals("time")) {
+                time = reader.nextString();
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
+        return new TrackerPos(pos, time);
+    }
 
 }
---
+++ app/src/main/java/com/conecta/restserver/TrackerPos.java
@@ -0,0 +1,36 @@
+package com.conecta.restserver;
+
+/**
+ * Created by 1765 IRON on 25/03/2018.
+ */
+
+public class TrackerPos {
+
+    String pos;
+    String time;
+
+    public TrackerPos() {
+
+    }
+
+    public TrackerPos(String pos, String time) {
+        this.pos = pos;
+        this.time = time;
+    }
+    public String getPos() {
+        return pos;
+    }
+    public void setPos(String pos) {
+        this.pos = pos;
+    }
+    public String getTime() {
+        return time;
+    }
+    public void setTime(String time) {
+        this.time = time;
+    }
+    @Override
+    public String toString() {
+        return "TrackerPos [pos=" + pos + ", time=" + time + "]";
+    }
+}
